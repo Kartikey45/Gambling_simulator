28fad1936e7713251c04b28614571dacb609f3ad
Kartikey45 <singhkartikey45@gmail.com>
2020-02-21 11:19:15 +0530

    ADDED:After 20 days of playing every day would like to know the total amount won or lost

--- Gambling_simulator.sh
+++ Gambling_simulator.sh
@@ -8,23 +8,41 @@ BET=1
 halfStake=$(($stake/2))
 lossLimit=$(($halfStake*$stake/$stake))
 gainLimit=$(($stake+$lossLimit))
+totalDays=20
+initialStake=100
+GainOrLoss=0
 
 #CHECK THE CONDITIONS FOR GAIN OR LOSS
-while [ $stake -ne $lossLimit ]
-do
-	if [ $stake -eq $gainLimit ]
+for((day=1; day<=$totalDays; day++))
+do	
+	while [ $stake -ne $lossLimit ]
+	do
+		if [ $stake -eq $gainLimit ]
+		then
+			break
+		fi
+		if [ $(( RANDOM%2 )) -eq $BET ]
+		then
+			stake=$(($stake+$BET)) 
+			(( win++ ))
+		else
+			stake=$(($stake-$BET))
+			(( loss-- ))
+		fi
+	done
+	echo "Daily stake changed : $stake"
+	if [ $stake -gt $initialStake ]
 	then
-		break
-	fi
-	if [ $(( RANDOM%2 )) -eq $BET ]
+		stakeToBe=$(($stake-$initialStake))
+	elif [ $stake -lt $initialStake ]
 	then
-		stake=$(($stake+$BET)) 
-		echo "wins !!!"	
-	else
-		stake=$(($stake-$BET))
-		echo "loss !!!"
+		stakeToBe=$(($stake-$initialStake))
 	fi
+	echo "Stake changed by :$stakeToBe"
+	echo ""
+	GainOrLoss=$(($GainOrLoss+$stakeToBe))
+	stake=100
 done
 
-#DISPLAY RESULTANT STACK
-echo "gambler's resultant stake is $stake rs."
+#DISPLAY TOTAL GAIN OR LOSS OF 20 DAYS
+echo "Overall gain or loss : $GainOrLoss"
