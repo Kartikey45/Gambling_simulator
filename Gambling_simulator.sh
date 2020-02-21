e54bf8f7980b06c0fe46802d4f6f2a882abf875d
Kartikey45 <singhkartikey45@gmail.com>
2020-02-21 11:10:33 +0530

    ADDED:Calculate if gambler won or lost 50% of stake, would resign for the day

--- Gambling_simulator.sh
+++ Gambling_simulator.sh
@@ -5,19 +5,26 @@ echo "*******************Wellcome to Gambling Simulator******************"
 #CONSTANT VARIABLE INITIALIZE
 stake=100
 BET=1
+halfStake=$(($stake/2))
+lossLimit=$(($halfStake*$stake/$stake))
+gainLimit=$(($stake+$lossLimit))
 
-#RANDOM VARIABLE INITIALIZE
-random=$(( RANDOM%2 ))
-
-#CHECK THE CONDITION 
-if [ $random -eq 1 ]
-then
-	stake=$(($stake+$BET)) 
-	echo "wins !!!"	
-else
-	stake=$(($stake-$BET))
-	echo "loss !!!"
-fi
+#CHECK THE CONDITIONS FOR GAIN OR LOSS
+while [ $stake -ne $lossLimit ]
+do
+	if [ $stake -eq $gainLimit ]
+	then
+		break
+	fi
+	if [ $(( RANDOM%2 )) -eq $BET ]
+	then
+		stake=$(($stake+$BET)) 
+		echo "wins !!!"	
+	else
+		stake=$(($stake-$BET))
+		echo "loss !!!"
+	fi
+done
 
 #DISPLAY RESULTANT STACK
-echo "$stake rs."
+echo "gambler's resultant stake is $stake rs."
