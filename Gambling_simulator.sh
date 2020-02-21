a20c089785a46e26de8cba63971c42a89f7fd0e7
Kartikey45 <singhkartikey45@gmail.com>
2020-02-21 11:03:04 +0530

    ADDED:Either win or loss for bet of 1 rs. only

--- Gambling_simulator.sh
+++ Gambling_simulator.sh
@@ -6,3 +6,18 @@ echo "*******************Wellcome to Gambling Simulator******************"
 stake=100
 BET=1
 
+#RANDOM VARIABLE INITIALIZE
+random=$(( RANDOM%2 ))
+
+#CHECK THE CONDITION 
+if [ $random -eq 1 ]
+then
+	stake=$(($stake+$BET)) 
+	echo "wins !!!"	
+else
+	stake=$(($stake-$BET))
+	echo "loss !!!"
+fi
+
+#DISPLAY RESULTANT STACK
+echo "$stake rs."
